749c76edc9534e736aadf8c686fd2857fa01870c
SANDHILLS\LH218-22 <jad.talbert@gmail.com>
2025-08-27 12:20:15 -0400

    lab 1, second commit

---
+++ favorite_movie_with_MCs.cpp
@@ -0,0 +1,12 @@
+//@author: Luis Bello
+//@date: 8/27/2025
+//@purpose: This programs prints the name of my favorite movies and its main character to the console
+
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    cout << "My favorite movie is Intersellat" endl << " The main character are " << endl;
+}
---
+++ square_root_of_real_number.cpp
@@ -0,0 +1,17 @@
+//@author: Luis Bello
+//@date: 8/27/2025
+//@purpose: This programs prints the square root of a number
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 16;
+    int results = sqrt(num1);
+
+    cout << results << endl;
+
+}
