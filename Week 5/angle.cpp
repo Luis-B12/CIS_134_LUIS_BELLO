363c5d11461fca1a5a8414f845cb7ef3c7878239
Luis-B12 <bellolg928@mail.sandhills.edu>
2025-09-15 14:32:43 -0400

    part 1

---
+++ Week 5/angle.cpp
@@ -0,0 +1,32 @@
+//@author: Luis Bello
+//@date: 9/15/2025
+//@purpose: This programs calculates and prints the angles of a right triangle
+
+#define _USE_MATH_DEFINES 
+#include <iostream>             //needed to define pi
+#include <cmath>                //needs to be included to sqrt and atan2()
+
+using namespace std;
+
+int main ()
+{
+    float x = 3,                //base of the triangle
+          c = 0,                //hypotenuse of the triagnle
+          y = 4,                //height of the traingle
+          theta = 0;            //angle between the base and hypotenuse
+
+    c = sqrt((x*x)+(y*y));      //Uses the pythergoean thereom to find c
+
+    cout << "C is " << c << "." << endl;    //Prints c
+
+    theta = atan2(y,x);         //Finds theta
+
+    cout << "Angle in radians: " << theta << endl;      //atan2 gives theta in radians
+    theta = theta * (180/M_PI);                         //M_PI is pi
+
+    cout << "Angle in degree: " << theta << endl;       //converts the angle to degrees
+
+return 0;
+
+
+}
---
+++ Week 5/infinite.cpp
@@ -0,0 +1,19 @@
+//@author: Luis Bello
+//@date: 9/15/2025
+//@purpose: This programs causes an infinite loop and prints it
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 1;
+
+    while (num < 2)
+    {
+        cout << "This is infinite.\n";
+    }    
+
+return 0;
+}
