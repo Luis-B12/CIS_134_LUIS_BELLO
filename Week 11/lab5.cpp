15b5974c56db2e69106864c8094d87b6b613d236
Luis-B12 <bellolg928@mail.sandhills.edu>
2025-11-02 13:22:56 -0500

    power

--- Week 11/lab5.cpp
+++ Week 11/lab5.cpp
@@ -17,11 +17,11 @@ int main()
 
     int array[size];
 
-    for(int num = 0; num < size; num++)
+    for(int num = 0; num < size; num++)         //goes through the loop to initiliaze the array
     {
         cout << "Please enter a interger" << endl;
 
-        while(!(cin >> array[num]))
+        while(!(cin >> array[num]))         //validates if the character inputted is a interger if the cin statement fails
         {
             cout << "You did NOT enter a integer. Try again." << endl;
             cin.clear();                        //clears the cin failure
@@ -32,24 +32,24 @@ int main()
     
     cout << "Integers inputted are: ";
     
-    for (int count = 0; count < size; count++)
+    for (int count = 0; count < size; count++)      //goes throught the numbers inputted
     {
     cout << array[count] << " ";
     }
 
     cout << endl;
 
-    int highest = array[0];
+    int highest = array[0];                         //the array sub 0 is set to the highest
 
-    for (int count = 0; count < size; count++)
+    for (int count = 0; count < size; count++)      //the loop goes through the array and comapres the other sectors with the sub 0 to find the highest value
     {
-        if(array[count] > highest)
+        if(array[count] > highest)                  //if a higher sector value is found
         {
-            highest = array[count];
+            highest = array[count];                 //initalize that value to highest
         }
     }
 
-    cout << "The largest integer is: " << highest << endl;
+    cout << "The largest integer is: " << highest << endl;      //print the highest
 
 return 0;
 }
